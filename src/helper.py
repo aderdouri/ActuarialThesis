d2c9b24e887bf442337f68e1abed9604127abeeb
Abderrazak DERDOURI <abderrazak.derdouri@gmail.com>
2023-05-21 20:22:21 +0200

    helper

--- src/helper.py
+++ src/helper.py
@@ -1,13 +1,24 @@
 import sys
 import subprocess
 
+# Evergrowing list of imports
+
+# The basics
 import pandas as pd
 import numpy as np
+from joblib import dump, load
+import re
 
+# The viz
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.colors import ListedColormap
 
+# Data preprocessing
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 # Metrics
 from sklearn.metrics import precision_recall_curve
@@ -18,6 +29,33 @@ from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.metrics import precision_score, recall_score
 from sklearn.metrics import matthews_corrcoef, confusion_matrix
+
+# Models
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+import xgboost.sklearn as xgb
+import lightgbm.sklearn as lgbm
+
+from sklearn.linear_model import LogisticRegression
+
+# Over- under- samplers
+from imblearn.combine import SMOTETomek
+from imblearn.over_sampling import ADASYN, RandomOverSampler
+from imblearn.under_sampling import NearMiss, RandomUnderSampler, TomekLinks
+
+# Clustering
+import hdbscan
+
+# Explaining
+from shap import TreeExplainer
+from shap import summary_plot
+import shap
+from lime.lime_tabular import LimeTabularExplainer
+
+# Models parameters tunning
+import optuna
+from optuna.integration import LightGBMPruningCallback
+
 
 
 def installALL():
