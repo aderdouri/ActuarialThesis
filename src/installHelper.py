e742ddc5f4607b2073ea45549209428fa056960b
Abderrazak DERDOURI <abderrazak.derdouri@gmail.com>
2023-05-21 20:48:36 +0200

    add install helper

--- src/installHelper.py
+++ src/installHelper.py
@@ -5,7 +5,7 @@ def installALL():
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'imbalanced-learn'])
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'catboost'])
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'hdbscan'])
-  subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'shape'])
+  subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'shap'])
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'lime'])
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'optuna'])
 
